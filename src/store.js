9c61cc0ed97d86daa7ab955fa19633bd340f5de0
brown-qs <77153068+brown-qs@users.noreply.github.com>
2021-02-01 22:32:42 +0700

    removed REDUX_DEVTOOLS_EXTENSION dependency
    No comment

--- src/store.js
+++ src/store.js
@@ -11,8 +11,9 @@ const store = createStore(
     rootReducer,
     initialState,
     compose(
-        applyMiddleware(middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
+        applyMiddleware(middleware)
+//         applyMiddleware(middleware),
+//         window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
     )
 );
 
